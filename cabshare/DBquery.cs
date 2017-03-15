bd7883de2475597c0f714b9358f388bab15759a7
suraj hanchinal <surajhanchinal@gmail.com>
2017-03-15 21:38:53 +0530

    added new classes for DBquery

---
+++ cabshare/DBquery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cabshare
+{
+    public class DBquery
+    {
+
+       // public static async  dataquery(LUIS data)
+        //{
+
+        //}
+    }
+}
