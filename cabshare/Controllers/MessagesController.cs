c8628f39808ecb4925df9e0c91e0e31c3fbf5674
suraj hanchinal <surajhanchinal@gmail.com>
2017-03-16 18:01:28 +0530

    test 7

--- cabshare/Controllers/MessagesController.cs
+++ cabshare/Controllers/MessagesController.cs
@@ -25,7 +25,7 @@ namespace cabshare
             {
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
                 string y = await ReplyCreate(activity);
-                Activity reply = activity.CreateReply((y.Split('\\'))[0]);
+                Activity reply = activity.CreateReply((y.Split(new string[] { Environment.NewLine }, StringSplitOptions.None))[0]);
                 await connector.Conversations.ReplyToActivityAsync(reply);
             }
             else
