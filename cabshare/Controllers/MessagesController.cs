a5f1be6affd2b697d7cc81bff42587869ceb6846
suraj hanchinal <surajhanchinal@gmail.com>
2017-03-16 15:29:01 +0530

    added microsoft bot credentials

--- cabshare/Controllers/MessagesController.cs
+++ cabshare/Controllers/MessagesController.cs
@@ -33,7 +33,7 @@ namespace cabshare
                 HandleSystemMessage(activity);
             }
             string x = await ReplyCreate(activity);
-            var response = Request.CreateResponse(HttpStatusCode.OK);
+            var response = Request.CreateResponse(x);
             return response;
 
             /*using (travelrecordEntities DB = new travelrecordEntities())
