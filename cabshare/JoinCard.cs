3bb4adecd71c167c25c3f525d808d8aff1b08623
suraj hanchinal <surajhanchinal@gmail.com>
2017-04-02 14:48:43 +0530

    small chanes

--- cabshare/JoinCard.cs
+++ cabshare/JoinCard.cs
@@ -20,7 +20,7 @@ namespace cabshare
         {
             foreach(var b in requests)
             {
-                string card = String.Format("DATE : {0}    TIME : {1}\r\nFROM : {2}    TO : {3}\r\nVACANCY : {4}    MEMBERS : {5}", b.date1.Value.ToShortDateString(), b.time1.ToString(), b.origin.TrimEnd(), b.destination.TrimEnd(),b.MAXNO,b.names);
+                string card = String.Format("DATE : {0}    TIME : {1}\r\nFROM : {2}    TO : {3}\r\nMEMBERS : {4}", b.date1.Value.ToShortDateString(), b.time1.ToString(), b.origin.TrimEnd(), b.destination.TrimEnd(),b.names);
                 Activity reply = activity.CreateReply("");
                 reply.Attachments = new List<Attachment>();
                 List<CardImage> cardImages = new List<CardImage>();
