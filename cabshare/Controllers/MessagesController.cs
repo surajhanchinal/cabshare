68a3d311377fae93b0d4621833dec61bf908845e
suraj hanchinal <surajhanchinal@gmail.com>
2017-04-02 13:29:54 +0530

    yes or no button

--- cabshare/Controllers/MessagesController.cs
+++ cabshare/Controllers/MessagesController.cs
@@ -89,19 +89,22 @@ namespace cabshare
                 message.From = botAccount;
                 message.Recipient = userAccount;
                 message.Conversation = new ConversationAccount(id: conversationId.Id);
-                message.Text = String.Format("{0} wants to join you Carpool.Select 'YES' or 'NO'",activity.From.Name);
+                message.Text = String.Format("{0} wants to join your Carpool. Select 'YES' or 'NO'",activity.From.Name);
                 message.Locale = "en-Us";
-                /*dynamic quickReplies = new JObject();
+                dynamic quickReplies = new JObject();
                 dynamic fbQRButtonRed = new JObject();
                 fbQRButtonRed.content_type = "text";
                 fbQRButtonRed.title = "YES";
                 fbQRButtonRed.payload = String.Format("{\"Answer\":\"YES\",\"Id\":\"{0}\",\"psid\":\"{1}\"",request.id,activity.From.Id);
+                fbQRButtonRed.image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Button_Icon_Red.svg/300px-Button_Icon_Red.svg.png";
                 dynamic fbQRButtonBlue = new JObject();
                 fbQRButtonBlue.content_type = "text";
                 fbQRButtonBlue.title = "NO";
                 fbQRButtonBlue.payload = String.Format("{\"Answer\":\"NO\",\"Id\":\"{0}\",\"psid\":\"{1}\"", request.id, activity.From.Id);
-                quickReplies.quick_replies = new JArray(fbQRButtonRed, fbQRButtonBlue);
-                message.ChannelData = quickReplies;*/
+                fbQRButtonBlue.image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Button_Icon_Blue.svg/768px-Button_Icon_Blue.svg.png";
+
+                quickReplies = new JArray(fbQRButtonRed, fbQRButtonBlue);
+                message.ChannelData = quickReplies;
                 await connector.Conversations.SendToConversationAsync((Activity)message);
                 return 1;
             }
