678d25978b795cadeede64f1e2b4efa57fc10eba
suraj hanchinal <surajhanchinal@gmail.com>
2017-04-04 11:25:38 +0530

    central indentation profile link

--- cabshare/JoinCard.cs
+++ cabshare/JoinCard.cs
@@ -44,7 +44,7 @@ namespace cabshare
                 HeroCard plCard = new HeroCard()
                 {
 
-                    Text = "Profile Link",
+                    Text = "       Profile Link",
                     Images = cardImages,
                     Buttons = cardButtons
                 };
